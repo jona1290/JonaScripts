68dffa492fd5eb4dc1acfda0215c63ec0c0575a3
jona1290 <62347705+jona1290@users.noreply.github.com>
2023-04-23 19:13:24 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 wget "https://linuxspecialist.blob.core.windows.net/bin/network-lab1?sp=r&st=2021-08-06T13:45:05Z&se=2030-08-06T21:45:05Z&spr=https&sv=2020-08-04&sr=b&sig=bxJznwO0zGDo8LiSLaqVsK6otUsrBp%2F8eUFoVmaw1OA%3D" -O network-lab
 chmod 755 network-lab
-./network-lab'
+./network-lab
